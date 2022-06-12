78cffde5695947c24dc299f003be095169a6cdcf
Brayan171020 <brayangt1710@gmail.com>
2022-06-11 20:32:55 -0400

    se añadio el widget filtro

--- lib/view/covid_page.dart
+++ lib/view/covid_page.dart
@@ -14,7 +14,7 @@ class CovidPage extends StatefulWidget {
 
 class _CovidPageState extends State<CovidPage> {
   final CovidBloc _newsBloc = CovidBloc();
-
+  final myController = TextEditingController();
   @override
   void initState() {
     _newsBloc.add(GetCovidList());
@@ -22,13 +22,40 @@ class _CovidPageState extends State<CovidPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('COVID-19 List')),
-      body: _buildListCovid(),
-    );
+  void dispose() {
+    // Limpia el controlador cuando el Widget se descarte
+    myController.dispose();
+    super.dispose();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('COVID-19 List')),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+              child: TextField(
+                //controller: controller,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: 'Covid',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    )),
+                //onChanged: buscarPais(),
+              ),
+            ),
+            Expanded(child: _buildListCovid())
+          ],
+        ));
+  }
+
+  //void buscarPais(String pais){
+  //  final suggestion =
+  //}
   Widget _buildListCovid() {
     return Container(
       margin: EdgeInsets.all(8.0),
@@ -69,32 +96,34 @@ class _CovidPageState extends State<CovidPage> {
       itemCount: model?.countries.length,
       itemBuilder: (context, index) {
         return Container(
-          margin: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(1),
+          margin: EdgeInsets.all(5.0),
           child: Row(
             children: [
               Container(
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(
+                height: 55,
+                width: 55,
+                decoration: BoxDecoration(
                     image: DecorationImage(
-                        image:
-                            NetworkImage('https://picsum.photos/250?image=9'),
-                        fit: BoxFit.cover),
+                        image: NetworkImage(toLink()), fit: BoxFit.cover),
                     shape: BoxShape.circle),
               ),
-              Card(
-                child: Container(
-                  margin: EdgeInsets.all(8.0),
-                  child: Column(
-                    children: <Widget>[
-                      Text("Country: ${model?.countries[index].country}"),
-                      Text(
-                          "Total Confirmed: ${model?.countries[index].totalConfirmed}"),
-                      Text(
-                          "Total Deaths: ${model?.countries[index].totalDeaths}"),
-                      Text(
-                          "Total Recovered: ${model?.countries[index].totalRecovered}"),
-                    ],
+              Expanded(
+                child: Card(
+                  child: Container(
+                    margin: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("Country: ${model?.countries[index].country}"),
+                        Text(
+                            "Total Confirmed: ${model?.countries[index].totalConfirmed}"),
+                        Text(
+                            "Total Deaths: ${model?.countries[index].totalDeaths}"),
+                        Text(
+                            "Total Recovered: ${model?.countries[index].totalRecovered}"),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -105,10 +134,13 @@ class _CovidPageState extends State<CovidPage> {
     );
   }
 
-  //BoxDecoration Imagenes() {
-  //  BoxDecoration imagen =
-  //      DecorationImage(image: NetworkImage("urlImage"), fit: BoxFit.cover);
-  //}
+  var count = 0;
+  String toLink() {
+    String link = 'https://picsum.photos/250?image=';
+
+    count++;
+    return link + count.toString();
+  }
 
   Widget _buildLoading() => Center(child: CircularProgressIndicator());
 }
